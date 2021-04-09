5ccd0c87e1ccf6a37f59345f5faaaf5038472f2c
geeeeeeeeeek <lujx1994@gmail.com>
2021-04-09 18:18:07 +0800

    Revert "chore: 实验室资源管理系统样式修改"
    This reverts commit 5a5ba0ff130efc2d44e7fadf83ad7c377b942bf5.

--- packages/admin/config/proxy.ts
+++ packages/admin/config/proxy.ts
@@ -8,7 +8,7 @@
 export default {
   dev: {
     '/api': {
-      target: 'http://127.0.0.1:5000',
+      target: 'http://localhost:5000',
       changeOrigin: true,
       pathRewrite: { '^': '' },
     },
